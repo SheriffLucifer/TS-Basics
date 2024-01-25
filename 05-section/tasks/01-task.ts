e7c1cd495623c4fa85500718677d61cdc3c1d85b
Oleg <olegashepelev@gmail.com>
2024-01-25 20:36:22 +0600

    Complete fifth section

--- 05-section/tasks/01-task.ts
+++ 05-section/tasks/01-task.ts
@@ -1,10 +1,11 @@
 interface ITruck {
-    brand: string;
-    model: string;
-    hasTrailer: boolean;
-  }
-  
-  // Выше описан интерфейс грузовика ITruck.
-  // Создайте тип обычного авто ICar убрав из
-  // интерфейса поле hasTrailer
-  
+  brand: string;
+  model: string;
+  hasTrailer: boolean;
+}
+
+// Выше описан интерфейс грузовика ITruck.
+// Создайте тип обычного авто ICar убрав из
+// интерфейса поле hasTrailer
+
+type IAuto = Omit<ITruck, "hasTrailer">;
--- 05-section/tasks/04-task.ts
+++ 05-section/tasks/04-task.ts
@@ -1,27 +1,26 @@
 interface IUserData {
-    firstName: string;
-    lastName: string;
-    age: number;
-    hometown: string;
-  }
-  
-  // Измените тип аргумента data в функции formattedUserData
-  // таким образом чтобы ее вызов ниже не вызывал ошибку
-  
-  function formattedUserData(data: IUserData): string {
-    return `
+  firstName: string;
+  lastName: string;
+  age: number;
+  hometown: string;
+}
+
+// Измените тип аргумента data в функции formattedUserData
+// таким образом чтобы ее вызов ниже не вызывал ошибку
+
+function formattedUserData(data: any): string {
+  return `
       Имя: ${data.firstName};
       Фамилия: ${data.lastName};
       Возраст: ${data.age};
       Родной город: ${data.hometown};
     `;
-  }
-  
-  console.log(
-    formattedUserData({
-      firstName: "Амир",
-      age: 25,
-      hometown: "Грозный"
-    })
-  );
-  
+}
+
+console.log(
+  formattedUserData({
+    firstName: "Амир",
+    age: 25,
+    hometown: "Грозный",
+  })
+);
