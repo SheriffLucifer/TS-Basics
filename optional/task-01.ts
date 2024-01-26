eb015fcf2d0fbdd0bd44f29409be9421bcaa122d
Oleg <olegashepelev@gmail.com>
2024-01-26 21:23:08 +0600

    Refactor first task in optional

--- optional/task-01.ts
+++ optional/task-01.ts
@@ -1,18 +1,7 @@
-enum Names {
-  alice = "Alice",
-  bob = "Bob",
-  zaphod = "Zaphod",
-}
-
-function giveCookie(name: Names): string {
-  switch (name) {
-    case Names.alice:
-      return `One for ${Names.alice}, one for me.`;
-    case Names.bob:
-      return `One for ${Names.bob}, one for me.`;
-    case Names.zaphod:
-      return `One for ${Names.zaphod}, one for me.`;
-    default:
-      return "One for you, one for me.";
+function shareCookie(name?: string): string {
+  if (name) {
+    return `One for ${name}, one for me.`;
+  } else {
+    return `One for you, one for me.`;
   }
 }
